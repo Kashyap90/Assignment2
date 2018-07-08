85d61908a06beaf8169fdf502a0894b12f47c708
Kashyap90 <40736782+Kashyap90@users.noreply.github.com>
2018-07-08 16:57:14 +0530

    Add files via upload

---
+++ For Loop pattern.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+n=5;
+for i in range(n):
+    for j in range(i):
+      print('*',end='')
+    print('')
+for i in range(n,0,-1):
+    for j in range(i):
+      print('*',end='')
+    print('')
+
