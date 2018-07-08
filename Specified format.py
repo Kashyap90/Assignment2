1080325ce67cf6e90f8595918534c5a9d70ecadb
Kashyap90 <40736782+Kashyap90@users.noreply.github.com>
2018-07-08 17:02:27 +0530

    Add files via upload

---
+++ Specified format.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+a = "WE, THE PEOPLE OF INDIA,"
+b = "having solemnly resolved to constitute India into a SOVEREIGN,!"
+c = "SOCIALIST, SECULAR, REPUBLIC"
+d = "and to secure to all its citizens"
+print(a, "\n\t",b, "\n\t\t",c, "\n\t\t",d, "\t\t\n")
+
